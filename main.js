add7b91cb6be2405c6c9e124bccaf01a858ca4db
zutin <gianluccaz@hotmail.com>
2024-05-13 11:57:57 -0300

    Tarefa - Cadê Buffet? VueJS

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const app = Vue.createApp({
   data(){
     return{
+      buffet: [],
+      eventList: [],
+      dateQuery: '',
       searchQuery: '',
-      listBuffets: []
+      eventAvailable: 0,
+      currentPage: 'index',
+      buffetList: []
     }
   },
   computed:{
     listResult(){
       if(this.searchQuery){
-        return this.listBuffets.filter(buffet => {
+        return this.buffetList.filter(buffet => {
           return buffet.trading_name.toLowerCase().includes(this.searchQuery.toLowerCase());
         })
       }else{
-        return this.listBuffets;
+        return this.buffetList;
       }
     }
   },
@@ -20,15 +25,61 @@ const app = Vue.createApp({
     this.listResult = await this.getData();
   },
   methods:{
+    seeMore(buffet){
+      this.buffet = buffet;
+      this.getEvents(buffet.id);
+      this.dateQuery = '';
+      this.eventAvailable = '';
+      this.currentPage = 'buffet';
+    },
+    async availabilityCheck(id, date){
+      let response = await fetch('http://127.0.0.1:3000/api/v1/events/'+id+'?date='+date+'&guests=10');
+      let data = await response.json();
+
+      if(data.available){
+        this.eventAvailable = 1
+      }else{
+        this.eventAvailable = 2
+      }
+    },
+    goBack(){
+      this.currentPage = 'index'
+      this.buffet = [];
+      this.eventList = [];
+    },
+    async getEvents(id){
+      let response = await fetch('http://127.0.0.1:3000/api/v1/buffets/'+id+'/events');
+      let data = await response.json();
+
+      this.eventList = [];
+
+      data.forEach(item => {
+        var event = new Object();
+        event.id = item.id;
+        event.name = item.name;
+        event.description = item.description;
+        event.minimum_participants = item.minimum_participants;
+        event.maximum_participants = item.maximum_participants;
+        event.default_duration = item.default_duration;
+        event.menu = item.menu
+        event.alcoholic_drinks = item.alcoholic_drinks;
+        event.decorations = item.decorations;
+        event.valet_service = item.valet_service;
+        event.can_change_location = item.can_change_location;
+
+        this.eventList.push(event);
+      });
+    },
     async getData(){
       let response = await fetch('http://127.0.0.1:3000/api/v1/buffets');
 
       let data = await response.json();
 
-      this.listBuffets = [];
+      this.buffetList = [];
 
       data.forEach(item => {
         var buffet = new Object();
+        buffet.id = item.id;
         buffet.trading_name = item.trading_name;
         buffet.contact_number = item.contact_number;
         buffet.email = item.email;
@@ -44,33 +95,10 @@ const app = Vue.createApp({
         }
         buffet.payment_methods = item.payment_methods;
 
-        this.listBuffets.push(buffet);
+        this.buffetList.push(buffet);
       });
     }
   }
 })
-
-// const app = Vue.createApp({
-//   data(){
-//     return{
-//       buffetName: 'Buffet 1',
-//       pic: 'https://randomuser.me/api/portraits/women/12.jpg',
-
-//       listBuffets: [
-//         {
-//           name: 'oi 1'
-//         },
-//         {
-//           name: 'oi 2'
-//         }
-//       ]
-//     }
-//   },
-//   methods:{
-//     changeData(){
-//       console.log('Dados alterados')
-//     }
-//   }
-// })
 
 app.mount('#app');
